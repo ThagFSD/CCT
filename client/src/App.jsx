bfa03e94642395a08033e4b57c578e967357ee4f
Nguyen Ngoc Thang <Thangnguyen1913@gmail.com>
2025-07-28 10:39:25 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowerRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Posts from './pages/Posts'
+export default function App() {
+
+  return (
+    <BrowerRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sig-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
+    </BrowerRouter>
+  )
+}
